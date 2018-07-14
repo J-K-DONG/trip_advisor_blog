431cbd57ef29f0cad77d3685913e490fe56755d1
DONG_JK <dongjinkun99@gmail.com>
2018-07-15 02:34:24 +0800

    commit messages

---
+++ src/com/advising/trip/user/User.java
@@ -0,0 +1,58 @@
+package com.advising.trip.user;
+
+public class User {
+	private int id;
+	private String name;
+	private String password;
+	private String sex;
+	private String phonenum;
+	private String location;
+	private String info;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhonenum() {
+		return phonenum;
+	}
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	
+	
+	
+}
---
+++ src/com/advising/trip/util/EncodingFilter.java
@@ -0,0 +1,40 @@
+package com.advising.trip.util;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ *  字符过滤器 解决字符乱码的问题
+ */
+public class EncodingFilter implements Filter {
+	
+	//构造函数
+    public EncodingFilter() {
+    	System.out.println("过滤器构造");
+    }
+
+    //过滤器修改方法
+	public void destroy() {
+		System.out.println("过滤器销毁");
+	}
+
+	//过滤器执行方法
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");//将编码改为utf-8
+		response.setContentType("text/html,charset=utf-8");
+		chain.doFilter(request, response);
+	}
+
+	//过滤器初始化方法
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("过滤器初始化");
+	}
+
+}
